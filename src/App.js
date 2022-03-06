f1841793518b473a9c3b401baec530536b1ac91f
Space-Belt <dngurwn@gmail.com>
2022-03-06 23:52:43 +0900

    [Feat] 폴더구조, 라우터설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
-  return <div className="App">안녕!</div>;
+  return (
+    <Router>
+      <GlobalStyles />
+      <Wrapper>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Wrapper>
+    </Router>
+  );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
 export default App;
